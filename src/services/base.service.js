9c5adba15fe07c753b07c87e12149181976a94fb
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-12-21 19:34:28 -0400

    adding commons services

--- src/services/base.service.js
+++ src/services/base.service.js
@@ -1,6 +1,11 @@
 class BaseService {
   constructor(repository) {
-    this.repository = repository;
+    this._repository = repository;
+  }
+
+  async getAll() {
+    const entities = await this._repository.getAll();
+    return entities;
   }
 
   async get(id) {
@@ -11,7 +16,46 @@ class BaseService {
       throw error;
     }
 
-    const currentEntity = await this.repository.get(id);
+    const entity = await this._repository.get(id);
+
+    if (!entity) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "entity does not found";
+      throw error;
+    }
+
+    return entity;
+  }
+
+  async create(entity) {
+    if (Object.keys(entity).length === 0 && entity.constructor === Object) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "Entity must be sent";
+      throw error;
+    }
+
+    const createdEntity = await this._repository.create(entity);
+    return createdEntity;
+  }
+
+  async update(id, entity) {
+    if (!id) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "id must be sent";
+      throw error;
+    }
+
+    if (Object.keys(entity).length === 0 && entity.constructor === Object) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "Entity must be sent";
+      throw error;
+    }
+
+    const currentEntity = await this._repository.get(id);
 
     if (!currentEntity) {
       const error = new Error();
@@ -20,11 +64,29 @@ class BaseService {
       throw error;
     }
 
-    return currentEntity;
+    const updatedEntity = await this._repository.update(id, entity);
+
+    return updatedEntity;
   }
 
-  async getAll() {
-    return await this.repository.getAll();
+  async delete(id) {
+    if (!id) {
+      const error = new Error();
+      error.status = 400;
+      error.message = "id must be sent";
+      throw error;
+    }
+
+    const deletedEntity = await this._repository.delete(id);
+
+    if (!deletedEntity) {
+      const error = new Error();
+      error.status = 404;
+      error.message = "entity does not found";
+      throw error;
+    }
+
+    return deletedEntity;
   }
 }
 
