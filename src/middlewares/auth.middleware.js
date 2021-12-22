930fd66fbbec51e10f7d25ffa488b07f368cb28e
Manuel Meneses <manuelmenesesg@hotmail.com>
2021-12-22 19:08:10 -0400

    adding authentication middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const { JWT_SECRET } = require("../config");
+
+module.exports = function (req, res, next) {
+  const token = req.headers["authorization"];
+
+  if (!token) {
+    const error = new Error();
+    error.status = 400;
+    error.message = "token must be sent";
+    throw error;
+  }
+
+  jwt.verify(token, JWT_SECRET, function (err, decodedToken) {
+    if (err) {
+      const error = new Error();
+      error.message = "Invalid token";
+      error.status = 401;
+      throw error;
+    }
+
+    req.user = decodedToken.user;
+    next();
+  });
+};
