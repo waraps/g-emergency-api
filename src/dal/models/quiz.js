e931f8285bf6e0c3cdf62cdd858a11f593dc520b
Manuel Meneses <manuelmenesesg@hotmail.com>
2022-01-11 17:11:50 -0400

    fix bug on keys

--- src/dal/models/quiz.js
+++ src/dal/models/quiz.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       Quiz.belongsToMany(models.Question, {
         through: "QuizQuestions",
-        foreignKey: "testId",
+        foreignKey: "quizId",
       });
     }
   }
