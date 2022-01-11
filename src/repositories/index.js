45011a6310d7775d87e031725005540c8c2d186e
Manuel Meneses <manuelmenesesg@hotmail.com>
2022-01-11 17:13:33 -0400

    adding quiz repository

--- src/repositories/index.js
+++ src/repositories/index.js
@@ -3,6 +3,7 @@ module.exports = {
   UserRepository: require("./user.repository"),
   QuestionRepository: require("./question.repository"),
   AnswerRepository: require("./answer.repository"),
+  QuizRepository: require("./quiz.repository"),
   ConsultationRepository: require("./consultation.repository"),
   PaymentRepository: require("./payment.repository"),
 };
